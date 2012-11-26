63b1b3b4ff419a5f5d55e3f4596faf26e8b538b0
Amit Kumar <toamitkumar@gmail.com>
2012-11-26 18:13:09 +0530

    Multiple curves have draggable points now

--- lib/plot/curve_plot.rb
+++ lib/plot/curve_plot.rb
@@ -1,10 +1,28 @@
 class CurvePlot
 
-  attr_accessor :delegate, :data, :categories, :graph, :draggable_point, :drag_point_selected, :dragged_to_y_coordinate, :number_of_series
-
+  attr_accessor :delegate, :graph, :draggable_points, :drag_point_selected, :dragged_to_y_coordinate, :curves, :drag_start
   def init
     if(super)
-      @data = (0..10).to_a
+      # @data = (0..10).to_a
+      @curves = [
+        {
+          :name => "TV",
+          :data => (0..10).to_a,
+          :color => CPTColor.orangeColor,
+          :static_point => 4,
+          :draggable_point => 3,
+          :fn => lambda {|p| 4 * p * p}
+        },
+        {
+          :name => "Umbrella_TV",
+          :data => (0..5).to_a,
+          :color => CPTColor.greenColor,
+          :static_point => 2,
+          :draggable_point => 3,
+          :fn => lambda {|p| 0.5 * p * p}
+        }
+      ]
+      @draggable_points = {}
     end
 
     self
@@ -38,47 +56,86 @@ class CurvePlot
 
 
   def numberOfRecordsForPlot(plot)
-    if(plot.identifier == "Curve")
-      @data.size
-    elsif(%w(DraggablePoint StaticPoint).include?(plot.identifier))
+    if(plot.identifier =~ /DraggablePoint/ or plot.identifier =~ /StaticPoint/)
       1
+    else
+      p plot.identifier
+      curve = @curves.select{|c| ("Curve_#{c[:name]}" == plot.identifier)}.first
+      curve[:data].size
+    # if(plot.identifier == "Curve")
+    #   @data.size
+    # elsif(%w(DraggablePoint StaticPoint).include?(plot.identifier))
+    #   1
+    # end
     end
   end
 
   def numberForPlot(plot, field:field_enum, recordIndex:index)
     num = nil
+    curve = @curves.select{|c| ("Curve_#{c[:name]}" == plot.identifier or "StaticPoint_#{c[:name]}" == plot.identifier or "DraggablePoint_#{c[:name]}" == plot.identifier)}.first
+    p plot.identifier
+    p curve[:fn]
+
     case field_enum
     when CPTScatterPlotFieldY
-      if(plot.identifier == "Curve")
+      if(plot.identifier =~ /Curve/)
         num = index
-        p "Curve Y - #{num}"
-      elsif(plot.identifier == "StaticPoint")
-        num = 4
-        p "Static Y - #{num}"
-      elsif(plot.identifier == "DraggablePoint")
-        if(@drag_point_selected)
+      elsif(plot.identifier =~ /StaticPoint/)
+        num = curve[:static_point]
+      elsif(plot.identifier =~ /DraggablePoint/)
+        if(@drag_start)
           num = @dragged_to_y_coordinate
         else
-          num = 3
+          num = curve[:draggable_point]
         end
-        p "Draggable Y - #{num}"
       end
     when CPTScatterPlotFieldX
-      if(plot.identifier == "Curve")
-        num = 4 * index * index
-        p "Curve X - #{num}"
-      elsif(plot.identifier == "StaticPoint")
-        num = 64
-        p "Static X - #{num}"
-      elsif(plot.identifier == "DraggablePoint")
-        if(@drag_point_selected)
-          num = 4 * @dragged_to_y_coordinate * @dragged_to_y_coordinate
+      if(plot.identifier =~ /Curve/)
+        num = curve[:fn].call(index)
+      elsif(plot.identifier =~ /StaticPoint/)
+        num = curve[:fn].call(curve[:static_point])
+      elsif(plot.identifier =~ /DraggablePoint/)
+        if(@drag_start)
+          num = curve[:fn].call(@dragged_to_y_coordinate)
         else
-          num = 36
+          num = curve[:fn].call(curve[:draggable_point])
         end
-        p "Draggable X - #{num}"
       end
     end
+
+
+    # case field_enum
+    # when CPTScatterPlotFieldY
+    #   if(plot.identifier == "Curve")
+    #     num = index
+    #     p "Curve Y - #{num}"
+    #   elsif(plot.identifier == "StaticPoint")
+    #     num = 4
+    #     p "Static Y - #{num}"
+    #   elsif(plot.identifier == "DraggablePoint")
+    #     if(@drag_point_selected)
+    #       num = @dragged_to_y_coordinate
+    #     else
+    #       num = 3
+    #     end
+    #     p "Draggable Y - #{num}"
+    #   end
+    # when CPTScatterPlotFieldX
+    #   if(plot.identifier == "Curve")
+    #     num = 4 * index * index
+    #     p "Curve X - #{num}"
+    #   elsif(plot.identifier == "StaticPoint")
+    #     num = 64
+    #     p "Static X - #{num}"
+    #   elsif(plot.identifier == "DraggablePoint")
+    #     if(@drag_point_selected)
+    #       num = 4 * @dragged_to_y_coordinate * @dragged_to_y_coordinate
+    #     else
+    #       num = 36
+    #     end
+    #     p "Draggable X - #{num}"
+    #   end
+    # end
     num
   end
 
@@ -108,9 +165,9 @@ class CurvePlot
     p x
     p y
 
-    if(@drag_point_selected)
+    if(@drag_start)
       @dragged_to_y_coordinate = y
-      @draggable_point.reloadData
+      @drag_point_selected.reloadData
     end
 
     true
@@ -124,20 +181,23 @@ class CurvePlot
 
   def plotSpace(space, shouldHandlePointingDeviceDownEvent:event, atPoint:point)
     p "shouldHandlePointingDeviceDownEvent"
-    @drag_point_selected = true
+    @drag_start = true
     false
   end
 
   def plotSpace(space, shouldHandlePointingDeviceUpEvent:event, atPoint:point)
     p "shouldHandlePointingDeviceUpEvent"
-    @drag_point_selected = false
+    @drag_start = false
     false
   end
 
   def scatterPlot(plot, plotSymbolWasSelectedAtRecordIndex:index)
     p "plotSymbolWasSelectedAtRecordIndex -- #{index}"
-    if(plot.identifier == "DraggablePoint")
-      @drag_point_selected = true
+    curve = @curves.select{|c| ("DraggablePoint_#{c[:name]}" == plot.identifier)}.first
+    if(curve)
+      @drag_start = true
+
+      @drag_point_selected = @draggable_points["DraggablePoint_#{curve[:name]}"]
     end
 
     true
@@ -207,56 +267,64 @@ class CurvePlot
   end
 
   def create_and_add_plot
-    curve = CPTScatterPlot.alloc.init
-    curve.identifier = "Curve"
-    # curve.interpolation = CPTScatterPlotInterpolationCurved
-    curve_line_style = CPTMutableLineStyle.lineStyle
-    curve_line_style.lineColor = CPTColor.orangeColor
-    curve_line_style.lineWidth = 5.0
-    
-    curve.dataLineStyle = curve_line_style
+    @curves.each do |c|
+      curve = CPTScatterPlot.alloc.init
+      curve.identifier = "Curve_#{c[:name]}"
+      # curve.interpolation = CPTScatterPlotInterpolationCurved
+      curve_line_style = CPTMutableLineStyle.lineStyle
+      curve_line_style.lineColor = c[:color]
+      curve_line_style.lineWidth = 5.0
+      
+      curve.dataLineStyle = curve_line_style
 
-    curve.dataSource = self
-    curve.delegate = self
-    @graph.addPlot(curve)
+      curve.dataSource = self
+      curve.delegate = self
+      @graph.addPlot(curve)
+    end
   end
 
   def create_and_add_static_point
-    static_point = CPTScatterPlot.alloc.initWithFrame(CGRectNull)
-    static_point.identifier = "StaticPoint"
+    @curves.each do |c|
+      static_point = CPTScatterPlot.alloc.initWithFrame(CGRectNull)
+      static_point.identifier = "StaticPoint_#{c[:name]}"
 
-    static_point_symbol_line_style = CPTMutableLineStyle.lineStyle
-    static_point_symbol_line_style.lineColor = CPTColor.orangeColor
+      static_point_symbol_line_style = CPTMutableLineStyle.lineStyle
+      static_point_symbol_line_style.lineColor = c[:color]
 
-    static_point_symbol = CPTPlotSymbol.ellipsePlotSymbol
-    static_point_symbol.fill = CPTFill.fillWithColor(CPTColor.orangeColor)
-    static_point_symbol.lineStyle = static_point_symbol_line_style
-    static_point_symbol.size = CGSizeMake(15.0, 15.0)
-    
-    static_point.plotSymbol = static_point_symbol
-    
-    static_point.dataSource = self
-    # static_point.delegate = self
-    @graph.addPlot(static_point)
+      static_point_symbol = CPTPlotSymbol.ellipsePlotSymbol
+      static_point_symbol.fill = CPTFill.fillWithColor(c[:color])
+      static_point_symbol.lineStyle = static_point_symbol_line_style
+      static_point_symbol.size = CGSizeMake(15.0, 15.0)
+      
+      static_point.plotSymbol = static_point_symbol
+      
+      static_point.dataSource = self
+      # static_point.delegate = self
+      @graph.addPlot(static_point)
+    end
   end
 
   def create_and_add_draggable_point
-    @draggable_point = CPTScatterPlot.alloc.initWithFrame(CGRectNull)
-    @draggable_point.identifier = "DraggablePoint"
+    @curves.each do |c|
+      draggable_point = CPTScatterPlot.alloc.initWithFrame(CGRectNull)
+      draggable_point.identifier = "DraggablePoint_#{c[:name]}"
 
-    draggable_point_symbol_line_style = CPTMutableLineStyle.lineStyle
-    draggable_point_symbol_line_style.lineColor = CPTColor.grayColor
+      draggable_point_symbol_line_style = CPTMutableLineStyle.lineStyle
+      draggable_point_symbol_line_style.lineColor = CPTColor.grayColor
 
-    draggable_point_symbol = CPTPlotSymbol.pentagonPlotSymbol
-    draggable_point_symbol.fill = CPTFill.fillWithColor(CPTColor.grayColor)
-    draggable_point_symbol.lineStyle = draggable_point_symbol_line_style
-    draggable_point_symbol.size = CGSizeMake(17.0, 17.0)
-    
-    @draggable_point.plotSymbol = draggable_point_symbol
-    
-    @draggable_point.dataSource = self
-    @draggable_point.delegate = self
-    @graph.addPlot(@draggable_point)
+      draggable_point_symbol = CPTPlotSymbol.pentagonPlotSymbol
+      draggable_point_symbol.fill = CPTFill.fillWithColor(CPTColor.grayColor)
+      draggable_point_symbol.lineStyle = draggable_point_symbol_line_style
+      draggable_point_symbol.size = CGSizeMake(17.0, 17.0)
+      
+      draggable_point.plotSymbol = draggable_point_symbol
+      
+      draggable_point.dataSource = self
+      draggable_point.delegate = self
+      @graph.addPlot(draggable_point)
+
+      @draggable_points["DraggablePoint_#{c[:name]}"] = draggable_point
+    end
   end
 
   def grid_line
